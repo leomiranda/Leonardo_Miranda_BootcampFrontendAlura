08e2711305334040cae3c797a6ef012d3c37cdb7
Leo Miranda <contato@leomiranda.com>
2021-08-07 16:32:30 -0400

    install eslint and prettier dev dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -5,25 +5,25 @@ import GlobalStyle from '../src/theme/GlobalStyle';
 import theme from '../src/theme';
 
 export default function App({ Component, pageProps }) {
-	return (
-		<>
-			<Head>
-				<title>Leo Miranda - Frontend Developer</title>
-				<link rel='preconnect' href='https://fonts.googleapis.com' />
-				<link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
-				<link
-					href='https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap'
-					rel='stylesheet'
-				/>
-				<link
-					href='https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:wght@300;400;700&display=swap'
-					rel='stylesheet'
-				/>
-			</Head>
-			<ThemeProvider theme={theme}>
-				<GlobalStyle />
-				<Component {...pageProps} />
-			</ThemeProvider>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Leo Miranda - Frontend Developer</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
