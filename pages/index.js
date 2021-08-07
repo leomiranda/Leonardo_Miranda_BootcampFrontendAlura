439494791dd67b236be8e844f63732558fe58956
Leo Miranda <contato@leomiranda.com>
2021-08-07 12:44:45 -0400

    create cover component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-	font-size: 50px;
-	color: ${({ theme }) => theme.colors.primary};
-`
+import Cover from '../src/components/commons/Cover'
 
 export default function Home() {
-	return <Title>Portfolio Leomiranda</Title>
+	return <Cover />
 }
---
+++ src/components/commons/Cover/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Cover(props) {
+	return (
+		<>
+			<h1>Leo Miranda</h1>
+			<h2>Frontend Developer</h2>
+		</>
+	)
+}
